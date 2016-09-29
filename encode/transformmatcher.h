d9a82a55cdfd2e0ec74e8a265b1c11fa9dec71ba
Sebastian Baginski <s.baginski@mail.com>
2016-09-29 21:01:13 +0200

    continue

--- encode/transformmatcher.h
+++ encode/transformmatcher.h
@@ -34,47 +34,49 @@ public:
 			/*fl 180*/{ -1, 0, 1, 0,  0, 1, 0, 0 },
 			/*fl 270*/{ 0, -1, 0, 1, -1, 0, 1, 0 }
 		};
-
 		transform_score_t result;
+
 		Transform t(Transform::Id);
 		do {
 			transform_score_t candidate;
 			candidate.distance = _metric.distance(a->image(), b->image(), t);
 			candidate.transform = t.type();
-			int _type = t.type();
 			if (candidate.distance <= result.distance) {
 				const double N = 4.0;
 				double sumA = 0.0, sumA2 = 0.0, sumB = 0.0, sumB2 = 0.0, sumAB = 0.0;
+				const Image::Pixel* source_b = b->image().data()->get();
+				const auto stride_b = b->image().stride();
+				const auto width_offset = __map_lookup[t.type()][2] * (b->width() - 1) + __map_lookup[t.type()][3] * (b->height() - 1);
+				const auto height_offset = __map_lookup[t.type()][6] * (b->width() - 1) + __map_lookup[t.type()][7] * (b->height() - 1);
 				for (uint32_t y = 0; y<2; ++y) {
+					const auto ys = (y * b->image().height()) / 2;
+					const auto y_width_offset = __map_lookup[t.type()][1] * ys + width_offset;
+					const auto y_height_offset = __map_lookup[t.type()][5] * ys + height_offset;
+					const auto y_width_offset_1 = __map_lookup[t.type()][1] * (ys + 1) + width_offset;
+					const auto y_height_offset_1 = __map_lookup[t.type()][5] * (ys + 1) + height_offset;
+
+					const auto map_x0 = __map_lookup[t.type()][0];
+					const auto map_x1 = __map_lookup[t.type()][4];
+					const auto y_off = y * a->image().stride();
+
 					for (uint32_t x = 0; x<2; ++x) {
-						const auto srcY = (y * b->image().height()) / 2;
-						const auto srcX = (x * b->image().width()) / 2;
-						const double valA = convert<double>(a->image().data()->get()[x + y * a->image().stride()]);
-						
-						
-						if (srcX == b->width() - 1)
-							--x;
-						if (srcY == b->height() - 1)
-							--y;
+						const double valA = convert<double>(a->image().data()->get()[x + y_off]);
+						auto xs = (x * b->image().width()) / 2;
+						const auto xs_x = map_x0 * xs;
+						const auto xs_y = map_x1 * xs;
+						const auto xs_x_1 = map_x0 * (xs + 1);
+						const auto xs_y_1 = map_x1 * (xs + 1);
+						auto tl = Point2d<uint32_t>(xs_x + y_width_offset, xs_y + y_height_offset);
+						auto tr = Point2d<uint32_t>(xs_x_1 + y_width_offset, xs_y_1 + y_height_offset);
+						auto bl = Point2d<uint32_t>(xs_x + y_width_offset_1, xs_y + y_height_offset_1);
+						auto br = Point2d<uint32_t>(xs_x_1 + y_width_offset_1, xs_y_1 + y_height_offset_1);
+						const int total = (int)source_b[tl.x() + tl.y() * stride_b]
+							+ (int)source_b[tr.x() + tr.y() * stride_b]
+							+ (int)source_b[bl.x() + bl.y() * stride_b]
+							+ (int)source_b[br.x() + br.y() * stride_b];
+						const Image::Pixel sample_b = (total / 4);
 
-						const auto _source = b->image().data()->get();
-						const auto _stride = b->image().stride();
-						const auto size = b->size();
-						auto tl = Point2d<uint32_t>(__map_lookup[_type][0] * srcX + __map_lookup[_type][1] * srcY + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * srcX + __map_lookup[_type][5] * srcY + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto tr = Point2d<uint32_t>(__map_lookup[_type][0] * (srcX + 1) + __map_lookup[_type][1] * srcY + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * (srcX + 1) + __map_lookup[_type][5] * srcY + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto bl = Point2d<uint32_t>(__map_lookup[_type][0] * srcX + __map_lookup[_type][1] * (srcY + 1) + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * srcX + __map_lookup[_type][5] * (srcY + 1) + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto br = Point2d<uint32_t>(__map_lookup[_type][0] * (srcX + 1) + __map_lookup[_type][1] * (srcY + 1) + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * (srcX + 1) + __map_lookup[_type][5] * (srcY + 1) + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-
-						const int total = (int)_source[tl.x() + tl.y() * _stride] + (int)_source[tr.x() + tr.y() * _stride] + (int)_source[bl.x() + bl.y() * _stride] + (int)_source[br.x() + br.y() * _stride];
-
-						const double valB = convert<double>(total / 4);
-						
-						
-						
+						const double valB = convert<double>(sample_b);
 						sumA += valA;
 						sumB += valB;
 						sumA2 += valA * valA;
@@ -107,47 +109,78 @@ public:
 			/*fl 180*/{ -1, 0, 1, 0,  0, 1, 0, 0 },
 			/*fl 270*/{ 0, -1, 0, 1, -1, 0, 1, 0 }
 		};
-
 		transform_score_t result;
+
 		Transform t(Transform::Id);
 		do {
 			transform_score_t candidate;
 			candidate.distance = _metric.distance(a->image(), b->image(), t);
 			candidate.transform = t.type();
-			int _type = t.type();
 			if (candidate.distance <= result.distance) {
-				const double N = 16.0;
+				const double N = (double)(a->image().width()) * a->image().height();
 				double sumA = 0.0, sumA2 = 0.0, sumB = 0.0, sumB2 = 0.0, sumAB = 0.0;
-				for (uint32_t y = 0; y<4; ++y) {
-					for (uint32_t x = 0; x<4; ++x) {
-						const auto srcY = (y * b->image().height()) / 4;
-						const auto srcX = (x * b->image().width()) / 4;
-						const double valA = convert<double>(a->image().data()->get()[x + y * a->image().stride()]);
+				const Image::Pixel* source_b = b->image().data()->get();
+				const auto stride_b = b->image().stride();
+				const auto width_offset = __map_lookup[t.type()][2] * (b->width() - 1) + __map_lookup[t.type()][3] * (b->height() - 1);
+				const auto height_offset = __map_lookup[t.type()][6] * (b->width() - 1) + __map_lookup[t.type()][7] * (b->height() - 1);
+				for (uint32_t y = 0; y<a->image().height(); ++y) {
+					const auto srcY = (y * b->image().height()) / a->image().height();
+					auto ys = srcY;
+					if (ys == b->height() - 1)
+						--ys;
+					const auto y_width_offset = __map_lookup[t.type()][1] * ys + width_offset;
+					const auto y_height_offset = __map_lookup[t.type()][5] * ys + height_offset;
+					const auto y_width_offset_1 = __map_lookup[t.type()][1] * (ys + 1) + width_offset;
+					const auto y_height_offset_1 = __map_lookup[t.type()][5] * (ys + 1) + height_offset;
 
+					const auto map_x0 = __map_lookup[t.type()][0];
+					const auto map_x1 = __map_lookup[t.type()][4];
+					const auto y_off = y * a->image().stride();
 
-						if (srcX == b->width() - 1)
-							--x;
-						if (srcY == b->height() - 1)
-							--y;
+					for (uint32_t x = 0; x<a->image().width(); ++x) {
 
-						const auto _source = b->image().data()->get();
-						const auto _stride = b->image().stride();
-						const auto size = b->size();
-						auto tl = Point2d<uint32_t>(__map_lookup[_type][0] * srcX + __map_lookup[_type][1] * srcY + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * srcX + __map_lookup[_type][5] * srcY + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto tr = Point2d<uint32_t>(__map_lookup[_type][0] * (srcX + 1) + __map_lookup[_type][1] * srcY + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * (srcX + 1) + __map_lookup[_type][5] * srcY + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto bl = Point2d<uint32_t>(__map_lookup[_type][0] * srcX + __map_lookup[_type][1] * (srcY + 1) + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * srcX + __map_lookup[_type][5] * (srcY + 1) + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
-						auto br = Point2d<uint32_t>(__map_lookup[_type][0] * (srcX + 1) + __map_lookup[_type][1] * (srcY + 1) + __map_lookup[_type][2] * (size.x() - 1) + __map_lookup[_type][3] * (size.y() - 1),
-							__map_lookup[_type][4] * (srcX + 1) + __map_lookup[_type][5] * (srcY + 1) + __map_lookup[_type][6] * (size.x() - 1) + __map_lookup[_type][7] * (size.y() - 1));
+						// mm_loadu_128(a->data() + y_off + x)
+						const double valA = convert<double>(a->image().data()->get()[x + y_off]);
 
-						const int total = (int)_source[tl.x() + tl.y() * _stride] + (int)_source[tr.x() + tr.y() * _stride] + (int)_source[bl.x() + bl.y() * _stride] + (int)_source[br.x() + br.y() * _stride];
+						// A = [ x, x+1, x+2, ..., x+7 ] = [ x, x, x, ..., x ] + [0, 1, 2, 3, 4, ..., 7]
+						// B = [ b / a, ..., b / a ]
+						// A * B
+						auto srcX = (x * b->image().width()) / a->image().width();
 
-						const double valB = convert<double>(total / 4);
+						// A = [0, ..., 1]
+						// mask = [0, ..., x+8 == b->w]
+						// r = r - A * mask
+						auto xs = srcX;
+						if (xs == b->width() - 1)
+							--xs;
 
+						// M0 = [ map_x0, ..., map_x0 ]
+						// M1 = [map_x1, ..., map_x1]
+						// _1 = [1, ..., 1]
 
+						// XS_X = M0 * r
+						const auto xs_x = map_x0 * xs;
+						// XS_Y = M1 * r
+						const auto xs_y = map_x1 * xs;
+						// XS_X_1 = M0 * (r + _1)
+						const auto xs_x_1 = map_x0 * (xs + 1);
+						// XS_Y_1 = M1 * (r + 1)
+						const auto xs_y_1 = map_x1 * (xs + 1);
 
+						// TL_X = XSX + [Y_W_OFF]
+						// TL_Y = (XSY + [Y_H_OFF]) * B_STRIDE
+						auto tl = Point2d<uint32_t>(xs_x + y_width_offset, xs_y + y_height_offset);
+						auto tr = Point2d<uint32_t>(xs_x_1 + y_width_offset, xs_y_1 + y_height_offset);
+						auto bl = Point2d<uint32_t>(xs_x + y_width_offset_1, xs_y + y_height_offset_1);
+						auto br = Point2d<uint32_t>(xs_x_1 + y_width_offset_1, xs_y_1 + y_height_offset_1);
+
+						const int total = (int)source_b[tl.x() + tl.y() * stride_b]
+							+ (int)source_b[tr.x() + tr.y() * stride_b]
+							+ (int)source_b[bl.x() + bl.y() * stride_b]
+							+ (int)source_b[br.x() + br.y() * stride_b];
+						const Image::Pixel sample_b = (total / 4);
+
+						const double valB = convert<double>(sample_b);
 						sumA += valA;
 						sumB += valB;
 						sumA2 += valA * valA;
