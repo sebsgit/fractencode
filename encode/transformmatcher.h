aedcfcedba13c22db2bc1e159dd3fee65ced1089
Sebastian Baginski <s.baginski@mail.com>
2016-09-29 20:14:29 +0200

    cleanup

--- encode/transformmatcher.h
+++ encode/transformmatcher.h
@@ -22,10 +22,9 @@ public:
 	{
 
 	}
-	transform_score_t match(const PartitionItemPtr& a, const PartitionItemPtr& b) const {
-		transform_score_t result;
 #ifdef FRAC_WITH_AVX
-
+	transform_score_t match_sse_8x8(const PartitionItemPtr& a, const PartitionItemPtr& b) const {
+		transform_score_t result;
 		static const int __map_lookup[8][8] = {
 			/*ID*/{ 1, 0, 0, 0,  0, 1, 0, 0 },
 			/*90*/{ 0, 1, 0, 0,  -1, 0, 1, 0 },
@@ -50,7 +49,7 @@ public:
 				const auto stride_b = b->image().stride();
 				const auto width_offset = __map_lookup[t.type()][2] * (b->width() - 1) + __map_lookup[t.type()][3] * (b->height() - 1);
 				const auto height_offset = __map_lookup[t.type()][6] * (b->width() - 1) + __map_lookup[t.type()][7] * (b->height() - 1);
-				
+
 				__m128i sumA_sse = _mm_setzero_si128();
 				__m128i x_increase_sse = _mm_set_epi16(7, 6, 5, 4, 3, 2, 1, 0);
 				__m128i map_x0_sse = _mm_set1_epi16(__map_lookup[t.type()][0]);
@@ -62,13 +61,7 @@ public:
 				uint16_t tr_store[8];
 				uint16_t bl_store[8];
 				uint16_t br_store[8];
-				Image::Pixel sample_b_sse[8];
-				double sumA_total_sse = 0.0;
-				double sumB_total_sse = 0.0;
-				double sumA2_total_sse = 0.0;
-				double sumB2_total_sse = 0.0;
-				double sumAB_total_sse = 0.0;
-				
+
 				for (uint32_t y = 0; y<a->image().height(); ++y) {
 					const auto srcY = (y * b->image().height()) / a->image().height();
 					auto ys = srcY;
@@ -95,19 +88,19 @@ public:
 						sumA_sse = _mm_add_epi16(x_sse, sumA_sse);
 						x_sse = _mm_mullo_epi16(x_sse, x_sse);
 						_mm_storeu_si128((__m128i*)tmp_sse, x_sse);
-						sumA2_total_sse += tmp_sse[0] + tmp_sse[1] + tmp_sse[2] + tmp_sse[3] + tmp_sse[4] + tmp_sse[5] + tmp_sse[6] + tmp_sse[7];
+						sumA2 += tmp_sse[0] + tmp_sse[1] + tmp_sse[2] + tmp_sse[3] + tmp_sse[4] + tmp_sse[5] + tmp_sse[6] + tmp_sse[7];
 
 						x_sse = _mm_set1_epi16(x);
 						x_sse = _mm_add_epi16(x_sse, x_increase_sse);
 
 						__m128i srcX_sse = _mm_mullo_epi16(x_sse, ab_ratio_sse);
-						srcX_sse = _mm_sub_epi16(srcX_sse, _mm_set_epi16(x+8 == b->width() ? 1 : 0, 0, 0, 0, 0, 0, 0, 0));
-					
+						srcX_sse = _mm_sub_epi16(srcX_sse, _mm_set_epi16(x + 8 == b->width() ? 1 : 0, 0, 0, 0, 0, 0, 0, 0));
+
 						__m128i xsx_sse = _mm_mullo_epi16(map_x0_sse, srcX_sse);
 						__m128i xsy_sse = _mm_mullo_epi16(map_x1_sse, srcX_sse);
 						__m128i xsx_sse_1 = _mm_mullo_epi16(map_x0_sse, _mm_add_epi16(srcX_sse, _mm_set1_epi16(1)));
 						__m128i xsy_sse_1 = _mm_mullo_epi16(map_x1_sse, _mm_add_epi16(srcX_sse, _mm_set1_epi16(1)));
-						
+
 						__m128i tl_x = _mm_add_epi16(xsx_sse, y_w_off_sse);
 						__m128i tl_y = _mm_add_epi16(xsy_sse, y_h_off_sse);
 						tl_y = _mm_mullo_epi16(tl_y, b_stride_sse);
@@ -133,103 +126,21 @@ public:
 						_mm_storeu_si128((__m128i*)bl_store, bl_x);
 						_mm_storeu_si128((__m128i*)br_store, br_x);
 
+						//TODO maybe vectorize this
 						for (int i = 0; i < 8; ++i) {
 							const int total = (int)source_b[tl_store[i]]
 								+ (int)source_b[tr_store[i]]
 								+ (int)source_b[bl_store[i]]
 								+ (int)source_b[br_store[i]];
-							sample_b_sse[i] = (total / 4);
-							sumAB_total_sse += convert<double>(total / 4) * src_data[i];
-							sumB2_total_sse += convert<double>(total / 4) * convert<double>(total / 4);
-							sumB_total_sse += convert<double>(total / 4);
+							sumAB += convert<double>(total / 4) * src_data[i];
+							sumB2 += convert<double>(total / 4) * convert<double>(total / 4);
+							sumB += convert<double>(total / 4);
 						}
 					}
-					/*
-					for (uint32_t x = 0; x<a->image().width(); ++x) {
-
-						// mm_loadu_128(a->data() + y_off + x)
-						const double valA = convert<double>(a->image().data()->get()[x + y_off]);
-
-						
-						// A = [ x, x+1, x+2, ..., x+7 ] = [ x, x, x, ..., x ] + [0, 1, 2, 3, 4, ..., 7]
-						// B = [ b / a, ..., b / a ]
-						// A * B
-						auto srcX = (x * b->image().width()) / a->image().width();
-
-						// A = [0, ..., 1]
-						// mask = [0, ..., x+8 == b->w]
-						// r = r - A * mask
-						auto xs = srcX;
-						if (xs == b->width() - 1)
-							--xs;
-
-						// M0 = [ map_x0, ..., map_x0 ]
-						// M1 = [map_x1, ..., map_x1]
-						// _1 = [1, ..., 1]
-						
-						// XS_X = M0 * r
-						const auto xs_x = map_x0 * xs;
-						// XS_Y = M1 * r
-						const auto xs_y = map_x1 * xs;
-						// XS_X_1 = M0 * (r + _1)
-						const auto xs_x_1 = map_x0 * (xs + 1);
-						// XS_Y_1 = M1 * (r + 1)
-						const auto xs_y_1 = map_x1 * (xs + 1);
-
-						// TL_X = XSX + [Y_W_OFF]
-						// TL_Y = (XSY + [Y_H_OFF]) * B_STRIDE
-						auto tl = Point2d<uint32_t>(xs_x + y_width_offset, xs_y + y_height_offset);
-						auto tr = Point2d<uint32_t>(xs_x_1 + y_width_offset, xs_y_1 + y_height_offset);
-						auto bl = Point2d<uint32_t>(xs_x + y_width_offset_1, xs_y + y_height_offset_1);
-						auto br = Point2d<uint32_t>(xs_x_1 + y_width_offset_1, xs_y_1 + y_height_offset_1);
-
-						const int total = (int)source_b[tl.x() + tl.y() * stride_b] 
-										+ (int)source_b[tr.x() + tr.y() * stride_b] 
-										+ (int)source_b[bl.x() + bl.y() * stride_b] 
-										+ (int)source_b[br.x() + br.y() * stride_b];
-						const Image::Pixel sample_b = (total / 4);
-		
-						const double valB = convert<double>(sample_b);
-						sumA += valA;
-						sumB += valB;
-						sumA2 += valA * valA;
-						sumB2 += valB * valB;
-						sumAB += valA * valB;
-					}*/
 				}
 
 				_mm_storeu_si128((__m128i*)tmp_sse, sumA_sse);
-				sumA_total_sse = tmp_sse[0] + tmp_sse[1] + tmp_sse[2] + tmp_sse[3] + tmp_sse[4] + tmp_sse[5] + tmp_sse[6] + tmp_sse[7];
-
-				sumA = sumA_total_sse;
-				sumB = sumB_total_sse;
-				sumA2 = sumA2_total_sse;
-				sumB2 = sumB2_total_sse;
-				sumAB = sumAB_total_sse;
-				/*if (sumA != sumA_total_sse) {
-					std::cout << sumA << ' ' << sumA_total_sse << '\n';
-					exit(0);
-				}
-
-				if (sumB != sumB_total_sse) {
-					std::cout << "sumB" << sumB << ' ' << sumB_total_sse << '\n';
-					exit(0);
-				}
-
-				if (sumA2 != sumA2_total_sse) {
-					std::cout << "sumA2" << sumA2 << ' ' << sumA2_total_sse << '\n';
-					exit(0);
-				}
-
-				if (sumB2 != sumB2_total_sse) {
-					std::cout << "sumB2" << sumB2 << ' ' << sumB2_total_sse << '\n';
-					exit(0);
-				}
-
-				if (sumAB != sumAB_total_sse) {
-					std::cout << "sumAB" << sumAB << ' ' << sumAB_total_sse << '\n';
-					exit(0);
-				}*/
+				sumA = tmp_sse[0] + tmp_sse[1] + tmp_sse[2] + tmp_sse[3] + tmp_sse[4] + tmp_sse[5] + tmp_sse[6] + tmp_sse[7];
 
 				const double tmp = (N * sumA2 - (sumA - 1) * sumA);
 				const double s = this->truncateSMax(fabs(tmp) < 0.00001 ? 0.0 : (N * sumAB - sumA * sumB) / tmp);
@@ -241,7 +152,11 @@ public:
 			if (this->checkDistance(result.distance))
 				break;
 		} while (t.next() != Transform::Id);
-#else
+		return result;
+	}
+#endif
+	transform_score_t match_default(const PartitionItemPtr& a, const PartitionItemPtr& b) const {
+		transform_score_t result;
 		Transform t(Transform::Id);
 		const SamplerBilinear samplerB(b->image());
 		do {
@@ -273,8 +188,16 @@ public:
 			if (this->checkDistance(result.distance))
 				break;
 		} while (t.next() != Transform::Id);
-#endif
 		return result;
+	}
+	inline transform_score_t match(const PartitionItemPtr& a, const PartitionItemPtr& b) const {
+#ifdef FRAC_WITH_AVX
+		if (a->width() == 8)
+			return this->match_sse_8x8(a, b);
+		return this->match_default(a, b);
+#else
+		return this->match_default(a, b);
+#endif
 	}
 	double truncateSMax(const double s) const noexcept {
 		if (_sMax > 0.0)
