4c71470f13f75fdf4304c55491faa8c94cfe5407
Sebastian Baginski <s.baginski@mail.com>
2019-08-11 17:15:31 +0200

    tests for codebook generator

--- tests/CodebookGeneratorTests.cpp
+++ tests/CodebookGeneratorTests.cpp
@@ -23,7 +23,24 @@ TEST_CASE("CodebookGenerator", "[encode]")
     }
     SECTION("basic float")
     {
+        const size_t dataSize = 2000;
+        const size_t codewordCount = 10;
+        const float rangeStart = -100.0f;
+        const float rangeEnd = 100.0f;
+        const float epsilon = 0.01f;
+        std::vector<float> data;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution dist(rangeStart, rangeEnd);
+        std::generate_n(std::back_inserter(data), dataSize, [&dist, &gen]() { return dist(gen); });
+        REQUIRE(data.size() == dataSize);
 
+        auto codebook = generateCodebook<float>(data.begin(), data.end(), codewordCount, epsilon, [](auto a, auto b) { return std::abs(a - b); });
+        REQUIRE(codebook.size() == codewordCount);
+        for (auto & c : codebook) {
+            REQUIRE(c >= rangeStart);
+            REQUIRE(c <= rangeEnd);
+        }
     }
 }
 
